325452ee0fbdc68a7cb9ed0c1627cfd672519db9
Apple <ellinemedrano1408@hotmail.com>
2021-05-03 18:20:38 +0200

    added Publisher entity and repository

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,24 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher penguin = new Publisher();
+        penguin.setName("Penguin Books Publishing");
+        penguin.setCity("Brussels");
+        penguin.setState("Belgium");
+
+        publisherRepository.save(penguin);
+        System.out.println("Publisher count: " + publisherRepository.count());
 
         Author elline = new Author("Elline", "Medrano");
         Book life = new Book("Life of Elline","14082001172");
@@ -37,7 +49,6 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(sofia);
         bookRepository.save(cutie);
 
-        System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
     }
 }
